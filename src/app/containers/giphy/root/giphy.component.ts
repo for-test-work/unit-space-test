cebfcb7b898f714faead5e9b2ba82fd70e8954bb
mykolai.lytvyn <Qwerdas74123>
2020-06-09 11:10:18 +0300

    first commit'

---
+++ src/app/containers/giphy/root/giphy.component.ts
@@ -0,0 +1,103 @@
+import {
+  Component, OnDestroy,
+  OnInit
+} from '@angular/core';
+
+import {
+  ActivatedRoute,
+  Router
+} from '@angular/router';
+
+// rxjs
+import {take, takeUntil} from 'rxjs/operators';
+
+// api
+import {GiphyService} from '../../../commons/api/giphy.service';
+
+// types
+import {GiphyItem, ResponseData} from '../../../commons/api/giphy.types';
+import {Subject} from 'rxjs';
+
+
+@Component({
+  selector: 'app-giphy',
+  templateUrl: './giphy.component.html',
+  styleUrls: ['./giphy.component.sass']
+})
+export class GiphyComponent implements OnInit, OnDestroy {
+
+
+  constructor(public giphyService: GiphyService,
+              private router: Router,
+              private route: ActivatedRoute) {
+    // todo... Misha
+    this.currentPageUrlParam = this.route.snapshot.params['page'];
+  }
+
+
+  private unsubscribe$ = new Subject<void>();
+
+  /*paginator*/
+  collectionSize = 0;
+  pageSize = 0;
+
+
+  foundItems: Array<GiphyItem> = [];
+
+  currentSearchUrlParam = '';
+  currentPageUrlParam = 1;
+
+  ngOnInit(): void {
+    this.currentSearchUrlParam = this.route.snapshot.params['search'];
+
+    this.giphyService.search(this.currentSearchUrlParam)
+      .pipe(take(1))
+      .subscribe(data => {
+        this.setControlsAndContentComponents(data);
+      });
+  }
+
+  changeRouteParams(searchParam: string, pageParam: number) {
+    this.router.navigate(['/giphy', {
+      search: searchParam,
+      page: pageParam
+    }]);
+  }
+
+  setControlsAndContentComponents(data: ResponseData) {
+    this.foundItems = data.data;
+    this.collectionSize = data.pagination.total_count;
+    this.pageSize = data.pagination.count;
+  }
+
+  onChangeControlsComponent($event) {
+    this.currentSearchUrlParam = $event.search;
+
+    this.giphyService.search(this.currentSearchUrlParam)
+      .pipe(takeUntil(this.unsubscribe$))
+      .subscribe(data => {
+        this.setControlsAndContentComponents(data);
+      });
+
+
+    this.changeRouteParams(this.currentSearchUrlParam, this.currentPageUrlParam);
+  }
+
+  onChangePagination($event: number) {
+    this.currentPageUrlParam = $event;
+
+    this.giphyService.search(this.currentSearchUrlParam, this.currentPageUrlParam)
+      .subscribe(data => {
+        this.setControlsAndContentComponents(data);
+      });
+
+    const offset = this.pageSize * this.currentPageUrlParam;
+    this.changeRouteParams(this.currentSearchUrlParam, this.currentPageUrlParam);
+  }
+
+  ngOnDestroy(): void {
+    this.unsubscribe$.next();
+    this.unsubscribe$.complete();
+  }
+
+}
